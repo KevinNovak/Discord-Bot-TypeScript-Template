01b80b42d386352a3526aaf32b774f6014571509
Kevin Novak <kevinmichaelnovak@gmail.com>
2020-11-03 17:09:26 -0500

    Move bot sites to separate config file

--- src/app.ts
+++ src/app.ts
@@ -48,7 +48,6 @@ async function start(): Promise<void> {
 
     let updateServerCountJob = new UpdateServerCountJob(
         Config.jobs.updateServerCount.schedule,
-        Config.botSites,
         shardManager,
         httpService
     );
--- src/jobs/update-server-count-job.ts
+++ src/jobs/update-server-count-job.ts
@@ -7,6 +7,7 @@ import { ShardUtils } from '../utils';
 import { Job } from './job';
 
 let Config = require('../../config/config.json');
+let BotSites: BotSite[] = require('../../config/bot-sites.json');
 let Logs = require('../../lang/logs.json');
 
 export class UpdateServerCountJob implements Job {
@@ -14,11 +15,10 @@ export class UpdateServerCountJob implements Job {
 
     constructor(
         public schedule: string,
-        botSites: BotSite[],
         private shardManager: ShardingManager,
         private httpService: HttpService
     ) {
-        this.botSites = botSites.filter(botSite => botSite.enabled);
+        this.botSites = BotSites.filter(botSite => botSite.enabled);
     }
 
     public async run(): Promise<void> {
