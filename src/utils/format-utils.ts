e75036de15e869347afe5ed89f7a6c1b1404e07a
Kevin Novak <kevinmichaelnovak@gmail.com>
2021-02-03 12:14:07 -0500

    Add roleMention()

--- src/utils/format-utils.ts
+++ src/utils/format-utils.ts
@@ -1,9 +1,13 @@
 export class FormatUtils {
-    public static userMention(discordId: string): string {
-        return `<@!${discordId}>`;
+    public static roleMention(discordId: string): string {
+        return `<@&${discordId}>`;
     }
 
     public static channelMention(discordId: string): string {
         return `<#${discordId}>`;
+    }
+
+    public static userMention(discordId: string): string {
+        return `<@!${discordId}>`;
     }
 }
