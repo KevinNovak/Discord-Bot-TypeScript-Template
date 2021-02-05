e5b10434886e468054fcf098dad3da9c57698134
Kevin Novak <kevinmichaelnovak@gmail.com>
2021-02-05 18:59:03 -0500

    Add StringUtils.truncate()

--- src/utils/string-utils.ts
+++ src/utils/string-utils.ts
@@ -1,6 +1,19 @@
 import removeMarkdown from 'remove-markdown';
 
 export class StringUtils {
+    public static truncate(input: string, length: number, addEllipsis: boolean = false): string {
+        if (input.length <= length) {
+            return input;
+        }
+
+        let output = input.substr(0, addEllipsis ? length - 3 : length);
+        if (addEllipsis) {
+            output += '...';
+        }
+
+        return output;
+    }
+
     public static stripMarkdown(input: string): string {
         return removeMarkdown(input);
     }
