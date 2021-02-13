a7b53f6ba1359ae9d0b784cd59067231ea3990ca
Kevin Novak <kevinmichaelnovak@gmail.com>
2021-02-12 22:30:47 -0500

    Reactions require READ_MESSAGE_HISTORY

--- src/utils/permission-utils.ts
+++ src/utils/permission-utils.ts
@@ -77,9 +77,12 @@ export class PermissionUtils {
 
         // VIEW_CHANNEL - Needed to view the channel
         // ADD_REACTIONS - Needed to add new reactions to messages
+        // READ_MESSAGE_HISTORY - Needed to add new reactions to messages
+        //    https://discordjs.guide/popular-topics/permissions-extended.html#implicit-permissions
         // MANAGE_MESSAGES - Needed to remove others reactions
         return channelPerms.has([
             Permissions.FLAGS.VIEW_CHANNEL,
+            Permissions.FLAGS.READ_MESSAGE_HISTORY,
             Permissions.FLAGS.ADD_REACTIONS,
             ...(removeOthers ? [Permissions.FLAGS.MANAGE_MESSAGES] : []),
         ]);
