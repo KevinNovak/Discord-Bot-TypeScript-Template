9f0c432c2aee5e24df0d08451ed6e00e1d7cdb9c
Kevin Novak <kevinmichaelnovak@gmail.com>
2021-02-23 23:54:43 -0500

    Add regex for matching regex

--- src/utils/regex-utils.ts
+++ src/utils/regex-utils.ts
@@ -1,4 +1,13 @@
 export class RegexUtils {
+    public static regex(input: string): RegExp {
+        let match = /^\/(.*)\/([^\/]*)$/.exec(input);
+        if (!match) {
+            return;
+        }
+
+        return new RegExp(match[1], match[2]);
+    }
+
     public static discordId(input: string): string {
         return input.match(/\b\d{17,20}\b/)?.[0];
     }
