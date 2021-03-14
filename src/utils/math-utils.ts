b23a68197ab3578f2b457545328e65f167af10dc
Kevin Novak <kevinmichaelnovak@gmail.com>
2021-03-14 01:43:24 -0500

    Add clamp() method to MathUtils

--- src/utils/math-utils.ts
+++ src/utils/math-utils.ts
@@ -3,6 +3,10 @@ export class MathUtils {
         return numbers.reduce((a, b) => a + b, 0);
     }
 
+    public static clamp(input: number, min: number, max: number): number {
+        return Math.min(Math.max(input, min), max);
+    }
+
     public static range(start: number, size: number): number[] {
         return [...Array(size).keys()].map(i => i + start);
     }
