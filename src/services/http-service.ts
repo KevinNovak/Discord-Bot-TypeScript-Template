9a74f8bfe0a31ed2dba3ae686a6cad746966431c
Kevin Novak <kevinmichaelnovak@gmail.com>
2021-03-29 12:06:35 -0400

    Add more methods to http service

--- src/services/http-service.ts
+++ src/services/http-service.ts
@@ -1,7 +1,8 @@
 import fetch, { Response } from 'node-fetch';
+import { URL } from 'url';
 
 export class HttpService {
-    public async get(url: string, authorization: string): Promise<Response> {
+    public async get(url: string | URL, authorization: string): Promise<Response> {
         return await fetch(url, {
             method: 'get',
             headers: {
@@ -11,9 +12,37 @@ export class HttpService {
         });
     }
 
-    public async post(url: string, authorization: string, body?: object): Promise<Response> {
+    public async post(url: string | URL, authorization: string, body?: object): Promise<Response> {
         return await fetch(url, {
             method: 'post',
+            headers: {
+                Authorization: authorization,
+                'Content-Type': 'application/json',
+                Accept: 'application/json',
+            },
+            body: body ? JSON.stringify(body) : null,
+        });
+    }
+
+    public async put(url: string | URL, authorization: string, body?: object): Promise<Response> {
+        return await fetch(url, {
+            method: 'put',
+            headers: {
+                Authorization: authorization,
+                'Content-Type': 'application/json',
+                Accept: 'application/json',
+            },
+            body: body ? JSON.stringify(body) : null,
+        });
+    }
+
+    public async delete(
+        url: string | URL,
+        authorization: string,
+        body?: object
+    ): Promise<Response> {
+        return await fetch(url, {
+            method: 'delete',
             headers: {
                 Authorization: authorization,
                 'Content-Type': 'application/json',
