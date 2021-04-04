d70d26fd44fd49e9022b5cda12fb039e8a3cc8a5
Kevin Novak <kevinmichaelnovak@gmail.com>
2021-04-04 13:51:46 -0400

    Refactor map-class

--- src/middleware/map-class.ts
+++ src/middleware/map-class.ts
@@ -1,22 +1,26 @@
-import { plainToClass } from 'class-transformer';
+import { ClassConstructor, plainToClass } from 'class-transformer';
 import { validate } from 'class-validator';
 import { NextFunction, Request, RequestHandler, Response } from 'express';
 
-export function mapClass(className: any): RequestHandler {
-    return (req: Request, res: Response, next: NextFunction) => {
-        const output: any = plainToClass(className, req.body);
-        validate(output, { skipMissingProperties: false }).then(errors => {
-            if (errors.length > 0) {
-                let errorTexts = Array();
-                for (const errorItem of errors) {
-                    errorTexts = errorTexts.concat(errorItem.constraints);
-                }
-                res.status(400).send(errorTexts);
-                return;
-            } else {
-                res.locals.input = output;
-                next();
+export function mapClass(cls: ClassConstructor<unknown>): RequestHandler {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        // Map to class
+        let output: any = plainToClass(cls, req.body);
+
+        // Validate class
+        let errors = await validate(output, { skipMissingProperties: false });
+        if (errors.length > 0) {
+            let errorTexts = Array();
+            for (const errorItem of errors) {
+                errorTexts = errorTexts.concat(errorItem.constraints);
             }
-        });
+
+            res.status(400).send(errorTexts);
+            return;
+        }
+
+        // Set validated class to locals
+        res.locals.input = output;
+        next();
     };
 }
