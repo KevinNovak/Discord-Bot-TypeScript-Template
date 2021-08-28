d8ffe977313998382977364b9512bef65b2a878f
Kevin Novak <kevinmichaelnovak@gmail.com>
2021-08-27 21:54:46 -0400

    Lint fixes

--- src/commands/command.ts
+++ src/commands/command.ts
@@ -1,4 +1,4 @@
-import { ApplicationCommandData, CommandInteraction, PermissionResolvable } from 'discord.js';
+import { CommandInteraction, PermissionResolvable } from 'discord.js';
 import { EventData } from '../models/internal-models';
 
 export interface Command {
