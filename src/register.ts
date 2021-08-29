a3b4f65d4119994319443b87e09fff385b38ce59
Kevin Novak <kevinmichaelnovak@gmail.com>
2021-08-29 16:00:08 -0400

    Alphabetize commands

--- src/register.ts
+++ src/register.ts
@@ -17,36 +17,26 @@ let Logs = require('../lang/logs.json');
 
 async function start(): Promise<void> {
     let cmdDatas: ApplicationCommandData[] = [
-        // Commands
+        DevCommand.data,
         HelpCommand.data,
         InfoCommand.data,
         LinkCommand.data,
         TestCommand.data,
         TranslateCommand.data,
-
-        // Developer Commands
-        DevCommand.data,
-    ];
+    ].sort((a, b) => (a.name > b.name ? 1 : -1));
 
     let cmdNames = cmdDatas.map(cmdInfo => cmdInfo.name);
 
     Logger.info(
         Logs.info.commandsRegistering.replaceAll(
             '{COMMAND_NAMES}',
-            cmdNames
-                .map(cmdName => `'${cmdName}'`)
-                .sort()
-                .join(', ')
+            cmdNames.map(cmdName => `'${cmdName}'`).join(', ')
         )
     );
 
     try {
         let rest = new REST({ version: '9' }).setToken(Config.client.token);
-
-        // Remove old commands
         await rest.put(Routes.applicationCommands(Config.client.id), { body: [] });
-
-        // Register new commands
         await rest.put(Routes.applicationCommands(Config.client.id), { body: cmdDatas });
     } catch (error) {
         Logger.error(Logs.error.commandsRegistering, error);
