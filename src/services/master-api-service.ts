47c5ed7c02ea8995fc8319c2c0d8282cc7905a25
Kevin Novak <kevinmichaelnovak@gmail.com>
2022-01-09 19:14:42 -0500

    Fix no-unsafe-return

--- src/services/master-api-service.ts
+++ src/services/master-api-service.ts
@@ -47,7 +47,7 @@ export class MasterApiService {
             throw res;
         }
 
-        return res.json();
+        return (await res.json()) as LoginClusterResponse;
     }
 
     public async ready(): Promise<void> {
