8a296d8ec2adda3f9debc129caf4d731446c2a9d
Kevin Novak <kevinmichaelnovak@gmail.com>
2022-01-13 21:03:32 -0500

    Replace switch with if-else

--- src/services/logger.ts
+++ src/services/logger.ts
@@ -43,42 +43,35 @@ export class Logger {
         }
 
         // Otherwise log details about the error
-        switch (true) {
-            case error instanceof Response: {
-                let resText: string;
-                try {
-                    resText = await error.text();
-                } catch {
-                    // Ignore
-                }
-                logger
-                    .child({
-                        path: error.url,
-                        statusCode: error.status,
-                        statusName: error.statusText,
-                        headers: error.headers.raw(),
-                        body: resText,
-                    })
-                    .error(message);
-                break;
+        if (error instanceof Response) {
+            let resText: string;
+            try {
+                resText = await error.text();
+            } catch {
+                // Ignore
             }
-            case error instanceof DiscordAPIError: {
-                logger
-                    .child({
-                        message: error.message,
-                        code: error.code,
-                        statusCode: error.httpStatus,
-                        method: error.method,
-                        path: error.path,
-                        stack: error.stack,
-                    })
-                    .error(message);
-                break;
-            }
-            default: {
-                logger.error(error, message);
-                break;
-            }
+            logger
+                .child({
+                    path: error.url,
+                    statusCode: error.status,
+                    statusName: error.statusText,
+                    headers: error.headers.raw(),
+                    body: resText,
+                })
+                .error(message);
+        } else if (error instanceof DiscordAPIError) {
+            logger
+                .child({
+                    message: error.message,
+                    code: error.code,
+                    statusCode: error.httpStatus,
+                    method: error.method,
+                    path: error.path,
+                    stack: error.stack,
+                })
+                .error(message);
+        } else {
+            logger.error(error, message);
         }
     }
 
