f0dc3d8e96cc8ae2ff3ebc895e4643d0a2af5bfa
Scott Bucher <47844014+scottbucher@users.noreply.github.com>
2022-04-18 20:40:50 -0400

    Updated error embed to have guild and shard id

--- src/events/command-handler.ts
+++ src/events/command-handler.ts
@@ -104,6 +104,8 @@ export class CommandHandler implements EventHandler {
                 intr,
                 Lang.getEmbed('errorEmbeds.command', data.lang(), {
                     ERROR_CODE: intr.id,
+                    GUILD_ID: intr.guild?.id ?? Lang.getRef('other.na', data.lang()),
+                    SHARD_ID: (intr.guild?.shardId ?? 0).toString(),
                 })
             );
         } catch {
