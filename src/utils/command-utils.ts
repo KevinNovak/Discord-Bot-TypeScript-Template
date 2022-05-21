1ece7587b609afde9eaff7414e075503b6c6c0c6
Kevin Novak <kevinmichaelnovak@gmail.com>
2022-05-20 20:18:48 -0400

    Add missing bool to return

--- src/utils/command-utils.ts
+++ src/utils/command-utils.ts
@@ -33,7 +33,7 @@ export class CommandUtils {
                         INTERVAL: FormatUtils.duration(command.cooldown.interval, data.lang()),
                     })
                 );
-                return;
+                return false;
             }
         }
 
