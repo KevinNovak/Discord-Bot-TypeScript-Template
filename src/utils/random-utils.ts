63ceb98bb74cae11596732a2a49e678983395976
Kevin Novak <kevinmichaelnovak@gmail.com>
2022-10-26 19:37:56 -0400

    Add RandomUtil.shuffle()

--- src/utils/random-utils.ts
+++ src/utils/random-utils.ts
@@ -2,4 +2,12 @@ export class RandomUtils {
     public static intFromInterval(min: number, max: number): number {
         return Math.floor(Math.random() * (max - min + 1) + min);
     }
+
+    public static shuffle(input: any[]): any[] {
+        for (let i = input.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [input[i], input[j]] = [input[j], input[i]];
+        }
+        return input;
+    }
 }
