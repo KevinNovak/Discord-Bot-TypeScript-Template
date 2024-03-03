a4b82a1d24b9dd82abf43852f8b22cf3a1bcb509
Kyle Venn <kvenn33@gmail.com>
2024-03-03 11:12:12 -0500

    Modernize code

--- src/events/voice-state-update-handler.ts
+++ src/events/voice-state-update-handler.ts
@@ -1,10 +1,12 @@
 import { NewsChannel, TextChannel, VoiceChannel, VoiceState } from 'discord.js';
 
 import { EventDataService, Logger } from '../services/index.js';
-import { ClientUtils } from '../utils/index.js';
+import { ClientUtils, MessageUtils } from '../utils/index.js';
 
 export class VoiceStateUpdateHandler {
+    // This can be used for querying a database
     private eventDataService: EventDataService;
+    private voiceChannelSuffix = '-time';
 
     constructor(eventDataService: EventDataService) {
         this.eventDataService = eventDataService;
@@ -18,61 +20,37 @@ export class VoiceStateUpdateHandler {
         if (oldState.channelId === newState.channelId) return;
 
         const guild = newState.guild;
-        // const channels = await this.listChannels(guild);
-        const channelAssocs: { [key: string]: string } = {
-            'game-time': 'games',
-            'riff-time': 'riffing',
-            General: 'general',
-        };
 
         let voiceChannel = await ClientUtils.getChannel(guild.client, newVoiceChannelId);
         // If it's not type voice channel, return
         if (!(voiceChannel instanceof VoiceChannel)) {
+            Logger.warn(`Channel ID ${newVoiceChannelId} is not a voice channel`);
             return;
         }
-        let associatedTextChannel: TextChannel | NewsChannel | null = null;
 
-        for (const [key, value] of Object.entries(channelAssocs)) {
-            if (voiceChannel.name.includes(key)) {
-                associatedTextChannel = await ClientUtils.findTextChannel(guild, value);
-                break;
-            }
-        }
+        const associatedTextChannel = await this.voiceChannelToTextChannel(voiceChannel);
         if (!associatedTextChannel) {
-            console.log(`No channel association found for channel ID ${newState.channelId}`);
+            Logger.warn(`No channel association found for channel ID ${newState.channelId}`);
             return;
         }
-        await associatedTextChannel.send(
+        await MessageUtils.send(
+            associatedTextChannel,
             `${newState.member.user.username} has joined the voice channel.`
         );
-
-        // if (voiceChannelId) {
-        //     const channelName = channels[voiceChannelId];
-        //     const chatChannelName = channelAssocs[channelName];
-        //
-        //     if (!chatChannelName) {
-        //         console.log(`No channel association found for channel ID ${newState.channelId}`);
-        //         return;
-        //     }
-        //
-        //     const user = await guild.members.fetch(newState.id);
-        //     const textChannel = guild.channels.cache.find(c => c.name === chatChannelName && c.type === 'GUILD_TEXT');
-        //     textChannel?.send(`${user.user.username} has joined the voice channel.`);
-        // }
     }
-    //
-    // private async listChannels(guild: any): Promise<{ [key: string]: string }> {
-    //     const results: { [key: string]: string } = {};
-    //     try {
-    //         const channels = await guild.channels.fetch();
-    //         channels.forEach((channel: any) => {
-    //             if (channel.type === 'GUILD_VOICE') {
-    //                 results[channel.id] = channel.name;
-    //             }
-    //         });
-    //     } catch (err) {
-    //         console.log('Error fetching guild channels:', err);
-    //     }
-    //     return results;
-    // }
+
+    private voiceChannelToTextChannel(
+        voiceChannel: VoiceChannel
+    ): Promise<TextChannel | NewsChannel> {
+        const voiceChannelName = voiceChannel.name;
+        const textChannelName = this.voiceChannelNameToTextChannelName(voiceChannelName);
+        return ClientUtils.findTextChannel(voiceChannel.guild, textChannelName);
+    }
+
+    // Every voice channel has a text channel associated with it based on suffix
+    // Ex. Voice Channel = game-time -> Text Channel = game
+    // Remove the `voiceChannelSuffix` from the voice channel name to get the text channel name
+    private voiceChannelNameToTextChannelName(voiceChannelName: string): string {
+        return voiceChannelName.replace(this.voiceChannelSuffix, '');
+    }
 }
