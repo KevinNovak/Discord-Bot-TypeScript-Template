1d4aded0e908edb8af894574c32ebb6eccdeb283
Kevin Novak <kevinmichaelnovak@gmail.com>
2025-08-22 18:39:56 -0400

    Discord now allows 250 pins per channel

--- src/constants/discord-limits.ts
+++ src/constants/discord-limits.ts
@@ -2,7 +2,7 @@ export class DiscordLimits {
     public static readonly GUILDS_PER_SHARD = 2500;
     public static readonly CHANNELS_PER_GUILD = 500;
     public static readonly ROLES_PER_GUILD = 250;
-    public static readonly PINS_PER_CHANNEL = 50;
+    public static readonly PINS_PER_CHANNEL = 250;
     public static readonly ACTIVE_THREADS_PER_GUILD = 1000;
     public static readonly EMBEDS_PER_MESSAGE = 10;
     public static readonly FIELDS_PER_EMBED = 25;
